aeb2553b57b2236f52a1a42caebb71adfcb4df30
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-12-07 19:23:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './sass/main.scss';
+import './css/styles.css';
+import fetchCountries from './fetchCountries.js';
+
+const DEBOUNCE_DELAY = 300;
+const countryInput = document.getElementById("search-box");
+const countryInfo = document.querySelector(".country-info");
+const countryList = document.querySelector(".country-list");
+
+countryInput.addEventListener("input", _.debounce(evt => {
+    if(countryInput.value.trim() === ""){
+        countryList.innerHTML = "";
+        countryInfo.innerHTML = "";
+        return;
+    }
+
+    fetchCountries(countryInput.value);
+}, DEBOUNCE_DELAY));
